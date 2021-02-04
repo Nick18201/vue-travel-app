9e2db628521e0b13ab02de4edadd23921c5948a7
nicolas blum <nblumferracci@gmail.com>
2021-02-04 16:07:41 +0100

    adding list of destination

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div v-for="destination in destinations" :key="destination.name">
+      <router-link :to="destination.slug">
+        <h2>{{ destination.name }}</h2>
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+// import HelloWorld from "@/components/HelloWorld.vue";
+import store from "@/store.js";
 export default {
   name: "Home",
-  components: {
-    HelloWorld
+  components: {},
+  data() {
+    return {
+      destinations: store.destinations
+    };
   }
 };
 </script>
