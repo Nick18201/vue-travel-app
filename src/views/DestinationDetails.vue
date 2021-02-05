ce65e54d2c7114b5c03c46735e2ed8a0c3b40790
nicolas blum <nblumferracci@gmail.com>
2021-02-05 10:15:33 +0100

    adding little styling

--- src/views/DestinationDetails.vue
+++ src/views/DestinationDetails.vue
@@ -9,6 +9,7 @@
         :src="require(`@/assets/${destination.image}`)"
         :alt="destination.name"
       />
+      <p>{{ destination.description }}</p>
     </div>
   </section>
 </template>
@@ -34,3 +35,21 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+img {
+  max-width: 600px;
+  height: auto;
+  width: 100%;
+  max-height: 400px;
+}
+.destination-details {
+  display: flex;
+  justify-content: space-between;
+}
+p {
+  margin: 0 40px;
+  font-size: 20px;
+  text-align: left;
+}
+</style>
