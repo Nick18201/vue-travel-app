329ff9c1a4edbbb7b0dac3afe22be48aeedfd9cd
nicolas blum <nblumferracci@gmail.com>
2021-02-05 11:17:08 +0100

    small styling to nav

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -56,4 +56,12 @@ img {
   display: flex;
   justify-content: space-between;
 }
+a {
+  color: lightseagreen;
+  text-decoration: none;
+}
+a:hover,
+a:visited {
+  text-decoration: underline;
+}
 </style>
